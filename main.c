fbb1f1dc2d6986acf33b82065e16940f085185a8
rohit244 <61743456+rohit244@users.noreply.github.com>
2020-03-17 16:50:24 +0530

    commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("-58356836578345843564874\n");
+    scanf("%d", &n);
+
+    if(n%2 == 0)
+        printf("even\n");
+    else
+        printf("odd\n");
+
+    return 0;
+
+}
