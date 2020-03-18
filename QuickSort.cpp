67baab34bd3e6b4e7be67613c69cf833ce3dd195
rohit244 <61743456+rohit244@users.noreply.github.com>
2020-03-18 10:15:35 +0530

    commit

---
+++ QuickSort.cpp
@@ -0,0 +1,57 @@
+#include<stdio.h>
+void q_s(int*,int,int);
+int conqure(int*,int,int);
+
+
+int main()
+{
+    int i,n,a[15];
+
+    printf("Range::");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++){
+	printf("Enter The Element :");
+        scanf("%d",&a[i]);}
+    q_s(a,0,n-1);
+    printf("sorted array is:::\n");
+    for(i=0;i<n;i++)
+        printf("%d\t",a[i]);
+
+    return 0;
+}
+
+void q_s(int a[],int l,int u)
+{
+
+    int index;
+    
+    if (l<u)
+    {
+        index=conqure(a,l,u);
+    q_s(a,l,index-1);
+    q_s(a,index+1,u);
+    }
+}
+
+int conqure(int a[],int start,int end)
+{
+    int pivot,i,j,t;
+    pivot=a[end];
+    i=start;
+    for(j=start;j<=end-1;j++)
+    {
+        if(a[j]<pivot)
+        {
+            
+            t=a[j];
+            a[j]=a[i];      /*swap*/
+            a[i]=t;
+            i++;
+        }
+    }
+     t=a[i];
+     a[i]=a[end];
+     a[end]=t;
+return i;
+}
